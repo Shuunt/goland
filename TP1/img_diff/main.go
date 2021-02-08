ae16f60f2ba17f5328d0186ba01389ea74646c2e
anthony.drogrez <anthony.drogrez@yahoo.com>
2021-02-08 14:03:42 +0100

    hello + crypto

--- TP1/img_diff/main.go
+++ TP1/img_diff/main.go
@@ -1,1 +1,27 @@
-package img_diff
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+func printContent(path string) {
+	content, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//fmt.Println(content)
+
+	h := sha256.New()
+
+	fmt.Println(h.Write(content))
+}
+
+func main() {
+	printContent("dummies/image_1.jpg")
+	//printContent("dummies/image_2.jpg")
+	//printContent("dummies/image_3.jpg")
+}
