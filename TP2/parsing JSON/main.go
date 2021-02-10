0ae398c80ffe8f89aebef20327b60def520cf880
anthony.drogrez <anthony.drogrez@yahoo.com>
2021-02-10 16:02:26 +0100

    TP2

--- TP2/parsing JSON/main.go
+++ TP2/parsing JSON/main.go
@@ -1,23 +1,31 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"os"
 )
 
 type User struct {
-	Login    string
-	Password string
+	userName string `json:"userName"`
+	Password string `json:"Password"`
 }
 
 func main() {
-	group := User{
-		Login:    "Paul",
-		Password: "pass123",
-	}
-	b, err := json.Marshal(group)
+	//group := User{
+	//	Login:    "Paul",
+	//	Password: "pass123",
+	//}
+	filepath, err := os.Open("users.json")
+
+	//Buffer
+	buffer := new(bytes.Buffer)
+	encoder := json.NewEncoder(buffer)
+	encoder.Encode(filepath)
+	//b, err := json.Marshal(buffer)
+
 	if err != nil {
 		fmt.Println("error:", err)
 	}
-	fmt.Println(string(b))
 }
